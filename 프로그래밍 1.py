5bc525c217693a02f65af145c0b3ba296aecb373
minchan8888 <93668370+minchan8888@users.noreply.github.com>
2026-03-05 19:45:05 +0900

    Add files via upload

---
+++ 프로그래밍 1.py
@@ -0,0 +1,23 @@
+print('hello python')
+def plus(a,b):
+    c=a+b
+    return c
+print = 4*plus(4,3)
+print(ans)
+
+print('hello','world')
+print('hello'+'world')
+print(100)
+print(2+4)
+
+x=10.8
+y=2.4
+
+x//y #몫
+x%y #나머지
+
+#변수를 이용하여 원의 면적과 둘레 구하기
+radius=6.0
+print('원의 반지름',radius)
+print('원의 면적',3.14*radius*radius)
+print('원의 둘레',2*3.14*radius)
